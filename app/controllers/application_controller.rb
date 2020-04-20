377511e7531b94b9db231a121ebda39a2325452d
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-04-20 15:18:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     user = User.find_by(:username => params[:username])
-    if user && user.authenticate 
+    if user && user.authenticate(params[:password])
   end
 
   get "/failure" do
