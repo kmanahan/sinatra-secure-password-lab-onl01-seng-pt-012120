7845670d68f4b1210e36b1b45d0d9a548af536d1
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-04-20 15:13:03 +0000

    Automatically backed up by Learn

--- db/migrate/20200420150839_create_user.rb
+++ db/migrate/20200420150839_create_user.rb
@@ -1,8 +1,8 @@
 class CreateUser < ActiveRecord::Migration[5.1]
   def change
     create_table :users |t|
-    t.string username
-    t.string :password_digest 
+    t.string: username
+    t.string: password_digest 
     end
   end
 end
